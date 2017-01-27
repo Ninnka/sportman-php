a0386453b67f54d1a8ec6856d501b780c2c1201a
Rennzh <465543032@qq.com>
2017-01-27 15:04:31 +0800

    backup

--- stadium_detail.php
+++ stadium_detail.php
@@ -14,9 +14,20 @@ $resultStatus = "";
 $resultData = "";
 
 $queryDetail = "select * from stadium where id='{$id}'";
+$queryDetailTrade = "SELECT stadium_tradedetail.* FROM stadium,stadium_tradedetail
+where stadium.id='{$id}' and stadium_tradedetail.id_stadium=stadium.id";
 $resArr = $conn->query($queryDetail);
 if(mysqli_affected_rows($conn) > 0){
     $resultData = mysqli_fetch_assoc($resArr);
 }
+
+$tradeArr = [];
+$resTradeList = $conn->query($queryDetailTrade);
+if(mysqli_affected_rows($conn) > 0){
+   for($i = 0;$i<mysqli_affected_rows($conn);$i++){
+       $tradeArr[] = mysqli_fetch_assoc($resTradeList);
+   }
+}
+$resultData["tradedetail"] = $tradeArr;
 
 echo json_encode(array("resultData"=>$resultData, "resultStatus"=>$resultStatus));
---
+++ stadium_detail_equipment.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MacyaRen
+ * Date: 2017/1/27
+ * Time: 14:32
+ */
+$id = $_POST["id"];
+
+$conn = mysqli_connect("localhost","root","","sportman");
+include 'UTF8.php';
+
+$resultStatus = "";
+$resultData = [];
+
+$queryEquipment = "SELECT stadium_equipment.* FROM stadium_equipment,stadium_tradedetail
+where stadium_tradedetail.id='{$id}' and stadium_equipment.id_stadium_tradedetail=stadium_tradedetail.id";
+$equipmentList = $conn->query($queryEquipment);
+if(mysqli_affected_rows($conn) > 0){
+    $resultStatus = "success";
+    for($i = 0; $i < mysqli_affected_rows($conn); $i++){
+        $resultData[] = mysqli_fetch_assoc($equipmentList);
+    }
+}else{
+    $resultStatus = "fail";
+}
+
+echo json_encode(array("resultData"=>$resultData,"resultStatus"=>$resultStatus));
