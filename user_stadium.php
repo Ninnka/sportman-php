7982e8abc32fdeb894e3575769c427095db42443
Rennzh <465543032@qq.com>
2017-01-27 23:50:43 +0800

    backup

---
+++ user_stadium.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MacyaRen
+ * Date: 2017/1/27
+ * Time: 15:39
+ */
+$id = $_POST["id"];
+
+$conn = mysqli_connect("localhost","root","","sportman");
+include 'UTF8.php';
+
+$resultStatus = "";
