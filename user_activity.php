5b11f963c631c1c291161e5dd5b967cd7edc049f
Rennzh <465543032@qq.com>
2017-01-28 15:19:31 +0800

    backup

--- user_activity.php
+++ user_activity.php
@@ -11,3 +11,19 @@ $status = $_POST["status"];
 $conn = mysqli_connect("localhost","root","","sportman");
 include 'UTF8.php';
 
+$resultStatus = "";
+$resultData = [];
+
+$queryActivity= "select activity.*,user_activity.registertime from activity,user_activity where user_activity.id_user = '{$id}' and user_activity.id_activity = activity.id and activity.status='{$status}'";
+$activityList= $conn->query($queryActivity);
+
+if(mysqli_affected_rows($conn) > 0){
+    $resultStatus = "success";
+    for($i=0;$i<mysqli_affected_rows($conn);$i++){
+        $resultData[] = mysqli_fetch_assoc($activityList);
+    }
+}else{
+    $resultStatus="fail";
+}
+
+echo json_encode(array("resultData"=>$resultData,"resultStatus"=>$resultStatus));
--- user_stadium.php
+++ user_stadium.php
@@ -6,8 +6,24 @@
  * Time: 15:39
  */
 $id = $_POST["id"];
+$status = $_POST["status"];
 
 $conn = mysqli_connect("localhost","root","","sportman");
 include 'UTF8.php';
 
 $resultStatus = "";
+$resultData = [];
+
+$queryStadium = "SELECT stadium.*,user_stadium.booktime,user_stadium.status from stadium,user_stadium where user_stadium.id_user='{$id}' and user_stadium.status = '{$status}' and user_stadium.id_stadium = stadium.id";
+$stadiumList = $conn->query($queryStadium);
+
+if(mysqli_affected_rows($conn) > 0){
+    $resultStatus = "success";
+    for($i=0;$i<mysqli_affected_rows($conn);$i++){
+        $resultData[] = mysqli_fetch_assoc($stadiumList);
+    }
+}else{
+    $resultStatus="fail";
+}
+
+echo json_encode(array("resultData"=>$resultData,"resultStatus"=>$resultStatus));
