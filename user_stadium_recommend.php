3e74c23ade2ad1944872ae9d8738b5fd3842c473
rennzh <hennzr@gmail.com>
2017-01-29 13:28:22 +0800

    backup

---
+++ user_stadium_recommend.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rainnka
+ * Date: 2017/1/29
+ * Time: 13:19
+ */
+$id = $_POST["id"];
+
+$conn = mysqli_connect("localhost", "root", "", "sportman");
+include 'UTF8.php';
+
+$queryStadium = "select stadium.* from stadium,user_stadium_recommend where user_stadium_recommend.id_user = '{$id}' and stadium.id = user_stadium_recommend.id_stadium";
+$resultStatus = "";
+$resultData = [];
+
+$stadiumList = $conn->query($queryStadium);
+if (mysqli_affected_rows($conn) > 0) {
+    $resultStatus = "success";
+    for ($i = 0; $i < mysqli_affected_rows($conn); $i++) {
+        $resultData[] = mysqli_fetch_assoc($stadiumList);
+    }
+} else {
+    $resultStatus = "fail";
+}
+
+echo json_encode(array("resultData"=>$resultData,"resultStatus"=>$resultStatus));
