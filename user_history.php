b2bdf8ab18a69d74ea7cbd695d3993d8ff2cb651
rennzh <hennzr@gmail.com>
2017-01-30 15:19:16 +0800

    backup

---
+++ user_history.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rainnka
+ * Date: 2017/1/30
+ * Time: 15:07
+ */
+$id = $_POST["id"];
+
+$conn = mysqli_connect("localhost", "root", "", "sportman");
+include 'UTF8.php';
+
+$resultStatus = "";
+$resultData = [];
+
+$queryHistory = "select * from user_history where user_history.id_user = '{$id}'";
+$historyList = $conn->query($queryHistory);
+if (mysqli_affected_rows($conn) > 0) {
+    $resultStatus = "success";
+    for ($i = 0; $i < mysqli_affected_rows($conn); $i++) {
+        $resultData[] = mysqli_fetch_assoc($historyList);
+    }
+} else {
+    $resultStatus = "fail";
+}
+
+echo json_encode(array("resultData"=>$resultData,"resultStatus"=>$resultStatus));
