03d5a125de87347595ab3cdcc8d9cb6ba818804a
rennzh <hennzr@gmail.com>
2017-02-02 23:10:48 +0800

    backup

---
+++ activity_addpayment.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rainnka
+ * Date: 2017/2/2
+ * Time: 15:05
+ */
+$id = $_POST["id"];
+$id_activity = $_POST["id_activity"];
+list($t1, $t2) = explode(' ', microtime());
+$timestamp = $t2 . ceil(($t1 * 1000));
+
+$conn = mysqli_connect("localhost", "root", "", "sportman");
+include 'UTF8.php';
+
+$resultData = "";
+$resultStatus = "";
+
+$queryDuplicate = "select * from user_payment_activity where id_user='{$id}' and id_activity='{$id_activity}'";
+$queryNumber = "select totalnumber,currentnumber from activity where id='{$id_activity}'";
+$queryAddPayment = "insert into user_payment_activity(id_user,id_activity,status,timestamp) VALUES('{$id}','{$id_activity}','待付款','{$timestamp}')";
+$queryUpdateNumber = "update activity set currentnumber=currentnumber+1 where id='{$id_activity}'";
+
+$conn->query($queryDuplicate);
+
+if (mysqli_affected_rows($conn) > 0) {
+    $resultStatus = "fail";
+    $resultData = "您已经申请了此活动";
+} else {
+    $tmpList = $conn->query($queryNumber);
+    $tmpArr = mysqli_fetch_array($tmpList);
+
+    if ($tmpArr["totalnumber"] > $tmpArr["currentnumber"]) {
+        $conn->query("SET AUTOCOMMIT=0");
+        $conn->begin_transaction();
+        if(!$conn->query($queryAddPayment)){
+            $resultStatus = "fail";
+            $resultData = "新建订单错误，申请失败";
+            $conn->rollback();
+        }
+        if(!$conn->query($queryUpdateNumber)){
+            $resultStatus = "fail";
+            $resultData = "更新人数错误，申请失败";
+            $conn->rollback();
+        }
+        $resultStatus = "success";
+        $resultData = "申请成功";
+        $conn->commit();
+    } else {
+        $resultStatus = "fail";
+        $resultData = "活动人数已满，申请失败";
+    }
+}
+
+echo json_encode(array("resultData" => $resultData, "resultStatus" => $resultStatus));
--- activity_detail.php
+++ activity_detail.php
@@ -6,6 +6,7 @@
  * Time: 16:23
  */
 $id = $_POST["id"];
+$id_activity = $_POST["id_activity"];
 
 $conn = mysqli_connect("localhost","root","","sportman");
 include 'UTF8.php';
@@ -13,7 +14,11 @@ include 'UTF8.php';
 $resultStatus = "";
 $resultData = "";
 
-$queryDetail = "select * from activity where id='{$id}'";
+$queryDetail = "select a.*,b.id AS recommended,c.id AS stared
+from activity a
+LEFT JOIN user_activity_recommend b on a.id = b.id_activity and b.id_user = '{$id}'
+LEFT JOIN user_activity_star c on a.id = c.id_activity and c.id_user = '{$id}'
+where a.id='{$id_activity}'";
 $resArr = $conn->query($queryDetail);
 if(mysqli_affected_rows($conn) > 0){
     $resultData = mysqli_fetch_assoc($resArr);
