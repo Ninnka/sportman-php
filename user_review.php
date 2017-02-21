6d4b66fd0c822ad37de66ea0cab91f54e7ddf891
Ninnka <hennzr@gmail.com>
2017-02-21 21:31:48 +0800

    修改获取的用户评论

--- user_review.php
+++ user_review.php
@@ -8,7 +8,7 @@
 include "access_allow_origin.php";
 
 $id = $_POST["id"];
-$status = $_POST["status"];
+$type = $_POST["type"];
 
 include 'connect_mysql.php';
 include 'UTF8.php';
@@ -16,32 +16,25 @@ include 'UTF8.php';
 $resultStatus = "";
 $resultData = [];
 
-$reviewActivityArr = [];
-$queryReviewActivity = "select activity.*,user_review.status from activity,user_review where user_review.id_user = '{$id}' and user_review.status = '{$status}' and activity.id = user_review.id_activity";
+$queryReview = "";
+switch($type){
+    case "activity":
+        $queryReview = "select activity.*,user_review.id_user,user_review.review,user_review.score,user_activity.registertime from activity,user_review,user_activity where user_review.id_user = '{$id}' and user_review.id_activity is not null and activity.id = user_review.id_activity and user_activity.id_user = user_review.id_user and user_activity.id_activity = activity.id";
+        break;
+    case "stadium":
+        $queryReview = "select stadium.*,user_review.id_user,user_review.review,user_review.score from stadium,user_review where user_review.id_user = '{$id}' and user_review.id_stadium is not null and stadium.id = user_review.id_stadium";
+        break;
+}
 
-$reviewActivityList = $conn->query($queryReviewActivity);
+$reviewList = $conn->query($queryReview);
 if (mysqli_affected_rows($conn) > 0) {
-    $resultStatus = "success";
     for ($i = 0; $i < mysqli_affected_rows($conn); $i++) {
-        $reviewActivityArr[] = mysqli_fetch_assoc($reviewActivityList);
+        $resultData[] = mysqli_fetch_assoc($reviewList);
     }
-} else {
-    $resultStatus = "fail";
-}
-$resultData["review_activity"] = $reviewActivityArr;
-
-$reviewStadiumArr = [];
-$queryReviewStadium = "select stadium.*,user_review.status from stadium,user_review where user_review.id_user = '{$id}' and user_review.status = '{$status}' and stadium.id = user_review.id_stadium";
-$reviewStadiumList = $conn->query($queryReviewStadium);
-if (mysqli_affected_rows($conn) > 0) {
     $resultStatus = "success";
-    for ($i = 0; $i < mysqli_affected_rows($conn); $i++){
-        $reviewStadiumArr[] = mysqli_fetch_assoc($reviewStadiumList);
-    }
 } else {
     $resultStatus = "fail";
 }
-$resultData["review_stadium"] = $reviewStadiumArr;
 
 echo json_encode(array("resultData" => $resultData, "resultStatus" => $resultStatus));
 
