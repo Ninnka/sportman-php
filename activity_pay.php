a6e7f8887e0b2faf0b3f624facf5c0b6a0156442
Ninnka <hennzr@gmail.com>
2017-02-22 21:31:50 +0800

    新增支付服务

---
+++ activity_pay.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lan
+ * Date: 2017/2/22
+ * Time: 下午8:27
+ */
+include "access_allow_origin.php";
+
+$id = $_POST["id"];
+
+include 'connect_mysql.php';
+include 'UTF8.php';
+
+$resultData = "";
+$resultStatus = "";
+
+$queryPay = "update user_payment_activity set status='已付款' where id='{$id}'";
+
+$conn->query($queryPay);
+if(mysqli_affected_rows($conn) > 0){
+    $resultData = "";
+    $resultStatus = "success";
+}else {
+    $resultData = "";
+    $resultStatus = "fail";
+}
+
+echo json_encode(array("resultData" => $resultData, "resultStatus" => $resultStatus));
+
+mysqli_close($conn);
