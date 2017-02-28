0e548c2a8eeff3644db7d5063f8003c9b160fdf5
Ninnka <hennzr@gmail.com>
2017-02-28 09:50:08 +0800

    修改连接数据库的信息

--- connect_mysql.php
+++ connect_mysql.php
@@ -5,4 +5,4 @@
  * Date: 2017/2/6
  * Time: 上午9:58
  */
-$conn = mysqli_connect("localhost", "root", "root", "sportman");
+$conn = mysqli_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS,SAE_MYSQL_DB);
